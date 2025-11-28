b420efa0d685a8c3516601dd6a14cb7fcdf38cc5
wengqianshan <wengqianshan@me.com>
2025-11-29 03:07:32 +0800

    fix reference page

--- apps/web/src/components/common/ScalarAPI/index.jsx
+++ apps/web/src/components/common/ScalarAPI/index.jsx
@@ -1,16 +1,22 @@
 'use client';
 
+import { useState } from 'react';
 import { useEffect } from 'react';
+import { Loading } from '../Loading';
 
 export default function ScalarAPI({ config }) {
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     // 加载 CDN 脚本
     const script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/npm/@scalar/api-reference';
+    // script.src = 'https://cdn.jsdelivr.net/npm/@scalar/api-reference';
+    script.src =
+      'https://unpkg.com/@scalar/api-reference@1.40.0/dist/browser/standalone.js';
     script.async = true;
 
     script.onload = () => {
       if (window.Scalar) {
+        setLoading(false);
         window.Scalar.createApiReference('#ScalarAPP', config);
       }
     };
@@ -20,5 +26,10 @@ export default function ScalarAPI({ config }) {
     return () => script.remove();
   }, []);
 
-  return <div id='ScalarAPP' />;
+  return (
+    <>
+      {loading && <Loading className='py-16' />}
+      <div id='ScalarAPP' />
+    </>
+  );
 }
