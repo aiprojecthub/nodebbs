4750e9439bf6fe223d6d6d3eac42350c737f0f60
wengqianshan <wengqianshan@me.com>
2025-12-10 00:41:41 +0800

    add log format

--- apps/api/src/app.js
+++ apps/api/src/app.js
@@ -1,20 +1,27 @@
 'use strict';
 
 import Fastify from 'fastify';
-import pino from 'pino';
 import server from './server.js';
 import { isDev } from './utils/env.js';
 
-const loggerInstance = pino({
+const logger = {
   level: isDev ? 'debug' : 'info',
-  transport: {
-    target: 'pino-pretty',
-  },
-});
+  transport: isDev
+    ? {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          translateTime: 'SYS:yyyy-mm-dd HH:MM:ss',
+          singleLine: false,
+        },
+      }
+    : undefined,
+};
 
 const app = Fastify({
-  loggerInstance,
+  logger,
   disableRequestLogging: true, // 禁用默认的请求日志
+  trustProxy: true,
 });
 
 app.register(server);
@@ -22,10 +29,10 @@ app.register(server);
 app.listen(
   { port: process.env.PORT || 7100, host: process.env.HOST || '0.0.0.0' },
   (err, address) => {
-    console.log(`服务启动成功，访问地址: ${address}`);
     if (err) {
       app.log.error(err);
       process.exit(1);
     }
+    app.log.info(`服务启动成功，访问地址: ${address}`);
   }
 );
