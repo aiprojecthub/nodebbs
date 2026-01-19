d44285637b03f15eae869ca2996b2324d8dc5745
wengqianshan <wengqianshan@me.com>
2026-01-19 17:59:04 +0800

    fix ad code

--- apps/web/src/app/(home)/layout.js
+++ apps/web/src/app/(home)/layout.js
@@ -12,18 +12,18 @@ export default async function HomeLayout({ children }) {
 
   return (
     <div className='container mx-auto py-3 sm:p-2 lg:px-4 lg:py-6 space-y-3'>
-      <AdSlot slotCode='header_banner' className='rounded-none lg:rounded-lg' />
+      <AdSlot slotCode='home_header_banner' className='rounded-none lg:rounded-lg' />
       <div className='flex lg:gap-6'>
         <div className='fixed z-10 -left-full lg:static lg:w-64 shrink-0'>
           <StickySidebar className='sticky top-[81px] space-y-4'>
-            <AdSlot slotCode='sidebar_top' />
+            <AdSlot slotCode='home_sidebar_top' />
             <Sidebar categories={categories} stats={stats} />
-            <AdSlot slotCode='sidebar_bottom' />
+            <AdSlot slotCode='home_sidebar_bottom' />
           </StickySidebar>
         </div>
         <main className='flex-1 min-w-0 overflow-hidden space-y-3'>{children}</main>
       </div>
-      <AdSlot slotCode='footer_banner' />
+      <AdSlot slotCode='home_footer_banner' />
     </div>
   );
 }
